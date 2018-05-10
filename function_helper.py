36594c28664b62f0662032ec0cb6b3fe01826da4
Jean Candelaria <jean.candelaria@upr.edu>
2018-05-10 19:07:40 -0400

    Fixed bug where 'AND' operator was not being recognized. Added code for generating truth tables and converting input to logical expression that can be evaluated.

--- function_helper.py
+++ function_helper.py
@@ -2,13 +2,15 @@ import sys
 import expression_converter as conv
 
 
-def function_parser(func, env):
+def function_parser(func, expression=None):
     if func == "TABLE":
-        generate_table(env)
+        generate_table(expression)
     elif func == "VENN":
-        generate_venn(env)
+        generate_venn(expression)
     elif func == "CKT":
-        generate_circuit(env)
+        generate_circuit(expression)
+    elif func == "SIMPLIFY":
+        simplify_expression(expression)
     elif func == "HELP":
         show_help()
     elif func == "EXIT":
@@ -19,11 +21,17 @@ def function_parser(func, env):
 
 def generate_table(env):
     print("The truth table is: ")
-    env.pop()
     expression = conv.env_to_expr(env)
-    print(expression)
     conv.generate_table(expression)
-    env.append(list())
+
+
+def simplify_expression(env):
+    print("Simplifying " + str(env[0]))
+    expression = conv.env_to_expr(env)
+    variables, minterms = conv.get_minterms(expression)
+    print(str(variables))
+    print(str(minterms))
+
 
 
 def generate_circuit(env):
@@ -35,7 +43,22 @@ def generate_venn(env):
 
 
 def show_help():
-    print("Here goes help with function descriptions.")
+    str = '''
+    To declare an expression:
+    out = OP input input ...
+    
+    Available OP's:
+    AND OR NOT XOR NOR NAND XNOR
+    
+    Available functions:
+    TABLE name - generate the truth table for the specified expression.
+    SIMPLIFY name - obtain a simplified version of the specified expression.
+    CKT name - draw the logic circuits for the specified expression.
+    VENN name - create a Venn diagram for the specified expression.
+    DEL name - remove created expression.
+    EXIT - close the program.
+          '''
+    print(str)
 
 
 def exit_case():
